9e6e9bc65c8cb7a93195029b5f92e9baf9c87573
DcodeDesign <gravythomas@gmail.com>
2020-12-17 09:23:34 +0100

    Initial commit

---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {MovieElement} from "../index";
+
+export default class MovieList extends Component {
+
+    render() {
+        return (
+            <div className={"w-75 d-flex flex-row flex-wrap align-content-start"}>
+                <MovieElement movie={this.props.movies[0]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                <MovieElement movie={this.props.movies[1]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                <MovieElement movie={this.props.movies[2]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                <MovieElement movie={this.props.movies[3]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+            </div>
+        );
+    }
+}
